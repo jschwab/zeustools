fab688c57d8972ba6009bf7b80a40d129ff5d90e
Josiah Schwab <jwschwab@berkeley.edu>
2011-05-06 11:58:28 -0700

    Suite of tools for interacting with ZeusMP2

---
+++ input.py
@@ -0,0 +1,427 @@
+from collections import OrderedDict
+
+class ZeusMPInput:
+
+### BEGIN ZEUS-MP2 NAMELIST TEMPLATE ###
+### see http://lca.ucsd.edu/portal/codes/zeusmp2/configuring for explanation of the parameters ###
+
+    _namelists = OrderedDict([
+
+    ("geomconf" , OrderedDict([
+         ("lgeom"  ,  1),
+         ("ldimen" ,  1)      ])),
+
+    ("physconf",  OrderedDict([
+         ("lrad"     , 0    ),
+         ("leos"     , 1    ),
+         ("npsec"    , 1    ),
+         ("xhydro"   , True ),
+         ("xforce"   , True ),
+         ("xmhd"     , False), 
+         ("xtotnrg"  , False), 
+         ("xgrav"    , False),  
+         ("xgrvfft"  , False), 
+         ("xptmass"  , False),
+         ("xiso"     , False), 
+         ("xsubav"   , False),
+         ("xvgrid"   , False)  ])),
+    
+    ("ioconf",    OrderedDict([
+         ("xascii"   , False),
+         ("xhdf"     , True ),	
+         ("xrestart" , False), 
+         ("xtsl"     , False)  ])),
+    
+    ("preconf"  , OrderedDict([
+         ("small_no" , "1.0D-99"),
+         ("large_no" , "1.0D+99") ])),
+    
+    ("arrayconf",  OrderedDict([
+         ("izones"   , 8 ),
+         ("jzones"   , 8 ),
+         ("kzones"   , 5 ), 
+         ("maxijk"   , 8 )     ])) ,
+
+    ("mpitop"   , OrderedDict([
+         ("ntiles(1)"   , 1),
+         ("ntiles(2)"   , 1),
+         ("ntiles(3)"   , 1),
+         ("periodic(1)" , False), 
+         ("periodic(2)" , False), 
+         ("periodic(3)" , False)  ])), 
+
+    ("rescon"     , OrderedDict([
+         ("irestart"   , 0),
+         ("resfile"    , None),
+                                ])),
+
+    ("pcon"     , OrderedDict([
+         ("nlim",   100000), 
+         ("tlim",   0.0   ), 
+         ("cpulim", 100000.0), 
+         ("mbatch", 0)         ])),
+
+    ("hycon"    , OrderedDict([
+         ("qcon"   , 2.0),
+         ("qlin"   , None),
+         ("courno" , 0.5)     ])),
+
+    ("iib"      ,OrderedDict([
+         ("niis(1)" , 1) ,
+         ("niis(2)" , None) ,
+         ("niis(3)" , None) ,
+         ("fiis(1)" , None) ,
+         ("fiis(2)" , None) ,
+         ("fiis(3)" , None) ,
+         ("fiis(4)" , None) ,
+         ("fiis(5)" , None) ,
+         ("fiis(6)" , None) ,
+         ("fiis(7)" , None) ,
+         ("fiis(8)" , None) ,
+         ("fiis(9)" , None) ,
+         ("fiis(10)" , None) ,
+         ("fiis(11)" , None) ,
+         ("fiis(12)" , None) ,
+         ("fiis(13)" , None) ])),
+
+    ("oib"      ,OrderedDict([
+         ("nois(1)" , 1) ,
+         ("nois(2)" , None) ,
+         ("nois(3)" , None) ,
+         ("fois(1)" , None) ,
+         ("fois(2)" , None) ,
+         ("fois(3)" , None) ,
+         ("fois(4)" , None) ,
+         ("fois(5)" , None) ,
+         ("fois(6)" , None) ,
+         ("fois(7)" , None) ,
+         ("fois(8)" , None) ,
+         ("fois(9)" , None) ,
+         ("fois(10)" , None) ,
+         ("fois(11)" , None) ,
+         ("fois(12)" , None) ,
+         ("fois(13)" , None) ])),
+
+    ("ijb"      ,OrderedDict([
+         ("nijs(1)" , 1) ,
+         ("nijs(2)" , None) ,
+         ("nijs(3)" , None) ,
+         ("fijs(1)" , None) ,
+         ("fijs(2)" , None) ,
+         ("fijs(3)" , None) ,
+         ("fijs(4)" , None) ,
+         ("fijs(5)" , None) ,
+         ("fijs(6)" , None) ,
+         ("fijs(7)" , None) ,
+         ("fijs(8)" , None) ,
+         ("fijs(9)" , None) ,
+         ("fijs(10)" , None) ,
+         ("fijs(11)" , None) ,
+         ("fijs(12)" , None) ,
+         ("fijs(13)" , None) ])),
+
+    ("ojb"      ,OrderedDict([
+         ("nojs(1)" , 1) ,
+         ("nojs(2)" , None) ,
+         ("nojs(3)" , None) ,
+         ("fojs(1)" , None) ,
+         ("fojs(2)" , None) ,
+         ("fojs(3)" , None) ,
+         ("fojs(4)" , None) ,
+         ("fojs(5)" , None) ,
+         ("fojs(6)" , None) ,
+         ("fojs(7)" , None) ,
+         ("fojs(8)" , None) ,
+         ("fojs(9)" , None) ,
+         ("fojs(10)" , None) ,
+         ("fojs(11)" , None) ,
+         ("fojs(12)" , None) ,
+         ("fojs(13)" , None) ])),
+
+    ("ikb"      ,OrderedDict([
+         ("niks(1)" , 1) ,
+         ("niks(2)" , None) ,
+         ("niks(3)" , None) ,
+         ("fiks(1)" , None) ,
+         ("fiks(2)" , None) ,
+         ("fiks(3)" , None) ,
+         ("fiks(4)" , None) ,
+         ("fiks(5)" , None) ,
+         ("fiks(6)" , None) ,
+         ("fiks(7)" , None) ,
+         ("fiks(8)" , None) ,
+         ("fiks(9)" , None) ,
+         ("fiks(10)" , None) ,
+         ("fiks(11)" , None) ,
+         ("fiks(12)" , None) ,
+         ("fiks(13)" , None) ])),
+
+    ("okb"      ,OrderedDict([
+         ("noks(1)" , 1) ,
+         ("noks(2)" , None) ,
+         ("noks(3)" , None) ,
+         ("foks(1)" , None) ,
+         ("foks(2)" , None) ,
+         ("foks(3)" , None) ,
+         ("foks(4)" , None) ,
+         ("foks(5)" , None) ,
+         ("foks(6)" , None) ,
+         ("foks(7)" , None) ,
+         ("foks(8)" , None) ,
+         ("foks(9)" , None) ,
+         ("foks(10)" , None) ,
+         ("foks(11)" , None) ,
+         ("foks(12)" , None) ,
+         ("foks(13)" , None) ])),
+
+    ("ggen1"    , OrderedDict([
+          ("nbl"    , 8),
+          ("x1min"  , 0.0),
+          ("x1max"  , 1.0),
+          ("igrid"  , 1),
+          ("x1rat"  , 1.0),
+          ("dx1min" , 1.0),
+          ("lgrid"  , True)   ])),
+
+
+    ("ggen2"    , OrderedDict([
+          ("nbl"    , 8),
+          ("x2min"  , 0.0),
+          ("x2max"  , 1.0),
+          ("igrid"  , 1),
+          ("x2rat"  , 1.0),
+          ("dx2min" , 1.0),
+          ("lgrid"  , True)   ])),
+
+
+    ("ggen3"    , OrderedDict([
+          ("nbl"    , 8),
+          ("x3min"  , 0.0),
+          ("x3max"  , 1.0),
+          ("igrid"  , 1),
+          ("x3rat"  , 1.0),
+          ("dx3min" , 1.0),
+          ("lgrid"  , True)   ])),
+
+
+    ("grvcon"   , OrderedDict([
+         ("guniv"  ,None),   
+         ("tgrav"  ,None),
+         ("ptmass" ,None),
+         ("x1ptm"  ,None),
+         ("x2ptm"  ,None),
+         ("x3ptm"  ,None),
+         ("xwedge" ,None)     ])),
+
+
+    ("radcon"   , OrderedDict([
+        ("fld",         None),
+        ("epsme",       None),
+        ("cnvcrit",     None),
+        ("ernom",       None),
+        ("ennom",       None),
+        ("demax",       None),
+        ("dermax",      None),
+        ("nmeiter",     None),
+        ("radth	",      None),
+        ("epsmaxd",     None),
+        ("cgerrcrit",   None),
+        ("ipcflag",     None),
+        ("xnu",         None),
+        ("powr",        None),
+        ("rho0",        None),
+        ("t_0",         None),
+        ("rmfp0",       None) ])),
+
+    ("eqos"     , OrderedDict([
+         ("gamma" , 1),
+         ("mmw"   , 1)        ])),
+
+    ("pgen"     , OrderedDict([
+                              ])),
+
+    ("gcon"     , OrderedDict([
+                              ])),
+
+    ("iocon"    , OrderedDict([
+         ("tusr",1.0), 
+         ("dtusr",1.0),
+         ("thdf",0.0),
+         ("dthdf",0.005e0)    ]))
+
+    ])  
+
+### END ZEUS-MP2 NAMELIST TEMPLATE ###
+
+
+    def __init__(self):
+        pass
+
+    def set_value(self, namelist, option, value):
+        try:
+            self._namelists[namelist][option] = value
+        except KeyError:
+            pass
+        
+        return
+
+    def get_value(self, namelist, option):
+        try:
+            retval = self._namelists[namelist][option]
+        except KeyError:
+            pass
+        
+        return retval
+
+
+    def set_geometry(self, lgeom = None, ldimen = None):
+        """Set coordinate system and number of dimensions"""
+        if lgeom:  self.set_value("geomconf" , "lgeom"  , lgeom)
+        if ldimen: self.set_value("geomconf" , "ldimen" , ldimen)
+        return
+
+    def set_nzones(self, izones = 1, jzones = 1, kzones = 1):
+        """Set number of zones on each tile"""
+        self.set_value("arrayconf", "izones", izones)
+        self.set_value("arrayconf", "jzones", jzones)
+        self.set_value("arrayconf", "kzones", kzones)
+        self.set_value("arrayconf", "izones", max([izones, jzones, kzones]))
+        return
+
+    def set_ntiles(self, itiles = 1, jtiles = 1, ktiles = 1):
+        """Set how many MPI tiles will be used"""
+        self.set_value("mpitop", "ntiles(1)", itiles)
+        self.set_value("mpitop", "ntiles(2)", jtiles)
+        self.set_value("mpitop", "ntiles(3)", ktiles)
+        return
+
+    def set_output(self):
+        """Choose output format and frequency"""
+        return
+
+    def set_mhd_bc(self, iis = None, ois = None , 
+                         ijs = None, ojs = None , 
+                         iks = None, oks = None):
+        """
+        Set MHD boundary conditions
+
+        Any of 6 mhd boundary conditions may be specified independently
+        at every zone on the physical problem boundary.  The boundary type is
+        specified by nflo, where
+
+            nflo = 0  =>  interior boundary (get data from neighboring tile)
+                 = 1  =>  reflecting (v(normal) = b(normal) = 0)
+                 =-1  =>  reflecting (XYZ: same as 1; ZRP: same as 1 with
+                          inversion of 3-components at ijb; RTP: same as 1
+                          with inversion of 2- and 3-components at iib and
+                          inversion of 3-components at ijb and ojb.)
+                 = 2  =>  flow out
+                 = 3  =>  flow in
+                 = 4  =>  periodic
+                 = 5  =>  reflecting (v(normal) = 0, b(tangential) = 0)
+
+        Note that in ZRP and RTP, some boundary conditions are implied by
+        the choice of limits.  e.g., if 0 .le. x3a .le. 2*pi in either ZRP
+        or RTP, then periodic boundary conditions should be imposed.
+        Set "niib" to -1 (reflecting with inversion of 2- and 3-components) 
+        if the inner i boundary is at the   origin (RTP).
+        Set "nijb" to -1 (reflecting with inversion of 3-components) if
+           the inner j boundary is on the "Z" axis (ZRP or RTP).
+        Set "nojb" to -1 (reflecting with inversion of 3-components) if
+           the outer j boundary is on the "Z" axis (RTP).
+        """
+
+        # set the appropriate boundary flags
+        if iis: self.set_value("iib" , "niis(1)" , iis)
+        if ois: self.set_value("oib" , "nois(1)" , ois)
+        if ijs: self.set_value("ijb" , "nijs(1)" , ijs)
+        if ojs: self.set_value("ojb" , "nojs(1)" , ojs)
+        if iks: self.set_value("ikb" , "niks(1)" , iks)
+        if oks: self.set_value("okb" , "noks(1)" , oks)
+        
+        # ensure that the appropriate MPI periodicities are set
+        if ( (iis == 4) or (ois == 4) ):
+            self.set_value("mpitop", "periodic(1)", True)
+        if ( (ijs == 4) or (ojs == 4) ):
+            self.set_value("mpitop", "periodic(2)", True)
+        if ( (iks == 4) or (oks == 4) ):
+            self.set_value("mpitop", "periodic(3)", True)
+        
+        return
+
+
+    def set_grav_bc(self, iis = None, ois = None , 
+                          ijs = None, ojs = None , 
+                          iks = None, oks = None):
+        """
+        Set gravity boundary conditions
+        
+        Boundary conditions on the gravitational potential are
+        specified by igr, where
+       
+          igr = 0  =>  interior boundary (get data from neighboring tile)
+              = 1  =>  reflecting (dgp/d(normal) = 0 "von Neumann")
+              = 2  =>  outflow (equivalent to reflecting)
+              = 3  =>  gp specified (Dirichlet)
+              = 4  =>  periodic
+       
+        The flags are defined by analogy with the hydro nflo flag.  This
+        is quite different from ZEUS-2D.  The flags igr are read in as:
+       
+         niis(3),nois(3),nijs(3),nojs(3),niks(3),noks(3)
+        """
+
+        # set the appropriate boundary flags
+        if iis: self.set_value("iib" , "niis(3)" , iis)
+        if ois: self.set_value("oib" , "nois(3)" , ois)
+        if ijs: self.set_value("ijb" , "nijs(3)" , ijs)
+        if ojs: self.set_value("ojb" , "nojs(3)" , ojs)
+        if iks: self.set_value("ikb" , "niks(3)" , iks)
+        if oks: self.set_value("okb" , "noks(3)" , oks)
+
+        return
+
+    def write(self, filename = "zmp_inp"):
+
+        # how to format the values
+        def _fmt_value(val):
+
+            if isinstance(val, bool):
+                if val:
+                    return ".TRUE."
+                else:
+                    return ".false."
+            elif isinstance(val, float):
+                return "{0:22.16E}".format(val)
+            else:
+                return str(val)
+
+        # get zmp file for writing
+        with open(filename, "w") as zmpfile:
+            
+            # header (add jobname, etc ?)
+            zmpfile.write(" #### ZEUSMP2 ZMP_INP AUTOGENERATED BY ZMPINP.PY #### \n")                       
+
+            for namelist, optdict in self.namelists.iteritems():
+            
+                nopts = len(optdict)
+
+                # open namelist
+                if nopts == 0:
+                    zmpfile.write("&{} /\n".format(namelist)) # empty options
+                else:
+                    zmpfile.write("&{}\n".format(namelist))
+            
+                for i, (option, value) in enumerate(optdict.iteritems()):
+                
+                    sep = ",\n"
+                    if (i == nopts - 1): sep = "/\n"
+                
+                    zmpfile.write("   {0:10} = {1:22}{2:1}".format(option, _fmt_value(value), sep))
+            
+
+
+if __name__ == "__main__":
+    z = ZeusMPInput()
+    z.set_value("geomconf", "lgeom", 3)
+    z.write()
