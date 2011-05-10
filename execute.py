810cc36d909279224618f6433fbaca1d28b4c56d
Josiah Schwab <jwschwab@berkeley.edu>
2011-05-10 16:33:06 -0700

    Among other things, support openmp

--- execute.py
+++ execute.py
@@ -1,4 +1,7 @@
+import glob
 import os
+import shlex
+import shutil
 import subprocess
 import sys
 
@@ -6,16 +9,20 @@ class ZeusMP:
 
     def __init__(self, rootdir = "./"):
         self.zeusdir = rootdir
+        self.exedir = "exe90"
 
-    def run(self):
+    def run(self, nproc = 1):
         """Run ZEUS-MP2 and post-process the output"""
 
-        fstdout = open(os.path.join(self.zeusdir, "exe90/zmp.stdout"), 'w')
+        fstdout = open(os.path.join(self.zeusdir, self.exedir, "zmp.stdout"), 'w')
 
         try:
-            subprocess.check_call(os.path.join(self.zeusdir, "exe90/zeusmp.x"),
-                                  cwd = os.path.join(self.zeusdir, "exe90"),
-                                  stdout = fstdout)
+            zmp_exe = os.path.join(self.zeusdir, self.exedir,"zeusmp.x")
+            zmp = shlex.split("openmpirun -n {} {}".format(nproc, zmp_exe))
+
+            subprocess.check_call(zmp, stdout = fstdout,
+                                  cwd = os.path.join(self.zeusdir, self.exedir))
+
         except (OSError, subprocess.CalledProcessError):
             print("Running Zeus Failed")
             print("Terminating...")
@@ -24,14 +31,14 @@ class ZeusMP:
         fstdout.close()
 
         try:
-            pp = subprocess.Popen(os.path.join(self.zeusdir, "exe90/zmp_pp.x"),
-                                  cwd = os.path.join(self.zeusdir, "exe90"),
+            pp = subprocess.Popen(os.path.join(self.zeusdir, self.exedir, "zmp_pp.x"),
+                                  cwd = os.path.join(self.zeusdir, self.exedir),
                                   stdin=subprocess.PIPE, 
                                   stdout = subprocess.PIPE)
         except:
             pass
         
-        nfiles = 1
+        nfiles = len(glob.glob(os.path.join(self.zeusdir, self.exedir, "hdfaa000000.*"))) - 1
         pp.communicate(input="auto_h5\n0\n{}\nquit\n".format(nfiles))
 
 
@@ -42,29 +49,31 @@ class ZeusMP:
         pass
 
 
-    def archive_files(archive_dir):
+    def archive(self, target_dir):
+        
+        def archive_file(filename, target_dir):
+            return os.rename(os.path.join(self.zeusdir,self.exedir,filename), 
+                             os.path.join(target_dir, filename))
+
+
 
         try:
-            os.mkdir(archive_dir)
+            os.mkdir(target_dir)
         except OSError:
-            shutil.rmtree(archive_dir)
-            os.mkdir(archive_dir)
+            shutil.rmtree(target_dir)
+            os.mkdir(target_dir)
 
-            hdf_files = glob.glob("hdfaa.*")
-            for hdf_file in hdf_files:
-                os.rename(hdf_file, os.path.join(archive_dir, hdf_file))
+        hdf_files = glob.glob(os.path.join(self.zeusdir, self.exedir, "hdfaa.*"))
+        for hdf_file in hdf_files:
+            hdf_filename = os.path.split(hdf_file)[1]
+            archive_file(hdf_filename, target_dir)
                 
-                shutil.copy("zmp_inp", os.path.join(archive_dir, "zmp_inp"))
-                shutil.copy("zmp_log", os.path.join(archive_dir, "zmp_log"))
+        for filename in ["zmp_log", "zmp_inp", "zmp.stdout"]:
+            archive_file(filename, target_dir)
 
-                return
-
-# remove any leftover output files
-
-    def cleanup():
-                
-        pre_files = glob.glob("hdfaa*.*")
+        pre_files = glob.glob(os.path.join(self.zeusdir, self.exedir,"hdfaa*.*"))
         for pre_file in pre_files:
             os.remove(pre_file)
-            
+
         return
+
