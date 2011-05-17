5f3731ed412f1d55c3282958dfa088614e0cf57c
Josiah Schwab <jwschwab@berkeley.edu>
2011-05-17 10:19:48 -0700

    Refactored configuration mechanism

--- execute.py
+++ execute.py
@@ -8,26 +8,30 @@ import sys
 
 class ZeusMP:
 
-    def __init__(self, rootdir = "./", makefile = "Makefile", mpiexec = "mpirun"):
-        self.zeusdir = rootdir
-        self.exedir = "exe90"
-        self.srcdir = "src90"
-        self.makefile = makefile
-        self.mpiexec = mpiexec
+    def __init__(self, zeus_config):
+        self.zeusdir = zeus_config.get("Directories", "rootdir")
+        self.exedir =  zeus_config.get("Directories", "exedir")
+        self.srcdir =  zeus_config.get("Directories", "srcdir")
+        self.makefile= zeus_config.get("Compilation", "Makefile")
+        self.mpiexec = zeus_config.get("Execution",   "mpiexec")
+
+        self.fullexedir = os.path.join(self.zeusdir, self.exedir)
+        self.fullsrcdir = os.path.join(self.zeusdir, self.srcdir)
 
     def run(self, nproc = 1):
         """Run ZEUS-MP2 and post-process the output"""
 
-        fstdout = open(os.path.join(self.zeusdir, self.exedir, "zmp.stdout"), 'w')
+        fstdout = open(os.path.join(self.fullexedir, "zmp.stdout"), 'w')
 
         try:
-            zmp_exe = os.path.join(self.zeusdir, self.exedir,"zeusmp.x")
+            # construct command
+            zmp_exe = os.path.join(self.fullexedir,"zeusmp.x")
             zmp = shlex.split("{} -n {} {}".format(self.mpiexec, nproc, zmp_exe))
 
-            subprocess.check_call(zmp, stdout = fstdout,
-                                  cwd = os.path.join(self.zeusdir, self.exedir))
+            subprocess.check_call(zmp, stdout = fstdout, cwd = self.fullexedir)
 
         except (OSError, subprocess.CalledProcessError):
+
             print("Running Zeus Failed")
             print("Terminating...")
             sys.exit(-1)
@@ -35,16 +39,16 @@ class ZeusMP:
         fstdout.close()
 
         try:
-            pp = subprocess.Popen(os.path.join(self.zeusdir, self.exedir, "zmp_pp.x"),
-                                  cwd = os.path.join(self.zeusdir, self.exedir),
-                                  stdin=subprocess.PIPE, 
+            # run included post-processing routine
+            pp = subprocess.Popen(os.path.join(self.fullexedir, "zmp_pp.x"),
+                                  cwd = self.fullexedir,
+                                  stdin  = subprocess.PIPE, 
                                   stdout = subprocess.PIPE)
         except:
             pass
         
-        nfiles = len(glob.glob(os.path.join(self.zeusdir, self.exedir, "hdfaa000000.*"))) - 1
+        nfiles = len(glob.glob(os.path.join(self.fullexedir, "hdfaa000000.*"))) - 1
         pp.communicate(input="auto_h5\n0\n{}\nquit\n".format(nfiles))
-
 
         return 
 
@@ -53,7 +57,7 @@ class ZeusMP:
 
         make_clean = "make -f {} clean".format(self.makefile)
         subprocess.check_call(shlex.split(make_clean), stdout = subprocess.PIPE,
-                              cwd = os.path.join(self.zeusdir, self.srcdir))
+                              cwd = self.fullsrcdir)
 
         return
 
@@ -62,25 +66,25 @@ class ZeusMP:
 
         make_newprob = "make -f {} newprob".format(self.makefile)
         subprocess.check_call(shlex.split(make_newprob), stdout = subprocess.PIPE,
-                              cwd = os.path.join(self.zeusdir, self.srcdir))
+                              cwd = self.fullsrcdir)
         
         # read in the makefile
-        with open(os.path.join(self.zeusdir, self.srcdir, self.makefile), "r") as mf:
+        with open(os.path.join(self.fullsrcdir, self.makefile), "r") as mf:
             lines = mf.readlines()
 
         # write it back out
-        with open(os.path.join(self.zeusdir, self.srcdir, self.makefile), "w") as mf:
+        with open(os.path.join(self.fullsrcdir, self.makefile), "w") as mf:
             for line in lines:
                 mf.write(re.sub("DPROBLEM=\w*","DPROBLEM={}".format(probname),line))
 
-
+        return
 
     def compile(self, newprob = False):
         """ Recompile ZEUS-MP2"""
 
         make_all = "make -f {} all".format(self.makefile)
         subprocess.check_call(shlex.split(make_all), stdout = subprocess.PIPE,
-                              cwd = os.path.join(self.zeusdir, self.srcdir))
+                              cwd = self.fullsrcdir)
 
         return
 
@@ -99,7 +103,7 @@ class ZeusMP:
             shutil.rmtree(target_dir)
             os.mkdir(target_dir)
 
-        hdf_files = glob.glob(os.path.join(self.zeusdir, self.exedir, "hdfaa.*"))
+        hdf_files = glob.glob(os.path.join(self.fullexedir, "hdfaa.*"))
         for hdf_file in hdf_files:
             hdf_filename = os.path.split(hdf_file)[1]
             archive_file(hdf_filename, target_dir)
@@ -107,7 +111,7 @@ class ZeusMP:
         for filename in ["zmp_log", "zmp_inp", "zmp.stdout"]:
             archive_file(filename, target_dir)
 
-        pre_files = glob.glob(os.path.join(self.zeusdir, self.exedir,"hdfaa*.*"))
+        pre_files = glob.glob(os.path.join(self.fullexedir,"hdfaa*.*"))
         for pre_file in pre_files:
             os.remove(pre_file)
 
@@ -115,6 +119,4 @@ class ZeusMP:
 
 
 if __name__ == "__main__":
-    z = ZeusMP("/Users/jschwab/Research/Software/zeusmp2", makefile = "Makefile.laptop", mpiexec = "openmpirun")
-    z.newprob("blast")
-    z.compile()
+    pass
